cdab18f78bf9baf40ec467ef540ffb6990d82890
Duncan Marsh <dwolfm@gmail.com>
2017-07-17 18:51:31 -0700

    update installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 git clone git@github.com:slugbyte/config.git ~/workspace/config
-cd ~/workspace
+cd ~/workspace/config 
 
 source ./config/.bashrc
 source ./plugin/dot.plugin.sh
