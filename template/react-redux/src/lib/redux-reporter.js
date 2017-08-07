78e6e56cf8e8636e80fa3fbc83a0e44a7c4221e1
Duncan Marsh <dwolfm@gmail.com>
2017-08-06 21:00:35 -0700

    added missing paren

--- template/react-redux/src/lib/redux-reporter.js
+++ template/react-redux/src/lib/redux-reporter.js
@@ -4,7 +4,7 @@ export default (store) => (next) => (action) => {
   util.log('__ACTION__', action)
   try {
     let result = next(action)
-    util.log('__STATE__', store.getState()
+    util.log('__STATE__', store.getState())
     return result
   } catch (err) {
     err.action = action
