474ff6d1c203ce4dae7464ef8963b05d27508a49
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-21 11:10:27 +0800

    update installer
    darwin installer cannon run on linux
    install.sh has logging

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash 
-[[ $(type dot) ]] || {
-  WORKSPACE=$HOME/workspace
-  mkdir -p $WORKSPACE/slug-bench
-  mkdir -p $WORKSPACE/node-bench
-  mkdir -p $WORKSPACE/rust-bench
-  mkdir -p $WORKSPACE/cf-bench
 
-  cd $HOME/workspace/slug-bench
-  git clone --recursive git@github.com:slugbyte/config.git 
-  cd config
-  source ./config/.bashrc
-  source ./plugin/dot.plugin.sh && dot sync 
-  source ./install/base.sh
+echo "INSTALLING DOT"
+echo "INIT WORKSPACE"
+WORKSPACE=$HOME/workspace
+mkdir -p $WORKSPACE/slug-bench
+mkdir -p $WORKSPACE/node-bench
+mkdir -p $WORKSPACE/rust-bench
+mkdir -p $WORKSPACE/cf-bench
 
-  select install_file in `ls ./install |grep -v base`; do 
-    source ./install/$install_file
-    break
-  done
-  echo DOT INSTALLED
-}
+
+echo "ENSURE DOT REPO"
+cd $HOME/workspace/slug-bench
+[[ -d config ]] || git clone --recursive git@github.com:slugbyte/config.git 
+
+echo "SOURCE DOT AND SYNC"
+cd config
+source ./config/.bashrc
+source ./plugin/dot.plugin.sh && dot sync 
+source ./install/base.sh
+
+echo "CHOOSE AN INSTALLER (n for none)"
+select install_file in `ls ./install |grep -v base`; do 
+  [[ -n $install_file ]] && source ./install/$install_file
+  break
+done
+echo DOT INSTALLED
--- install/osx.sh
+++ install/osx.sh
@@ -1,4 +1,8 @@
 # if no brew install brew 
-[[ $(which brew) ]] || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+[[ $(uname -s) = 'Darwin' ]] && {
+  [[ $(which brew) ]] || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install vim tmux htop pstree git httpie aria2 wget nvm fzf nmap reattach-to-user-namespace the_silver_searcher
+  brew install vim tmux htop pstree git httpie aria2 wget nvm fzf nmap reattach-to-user-namespace the_silver_searcher
+}  || {
+  echo 'ERROR: not darwin cannot install'
+}
