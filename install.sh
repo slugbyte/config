2dc5d382737a74feaec2c90e9138b99e79d4cd38
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-21 10:58:11 +0800

    update installer with install scripts

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash 
+[[ $(type dot) ]] || {
+  WORKSPACE=$HOME/workspace
+  mkdir -p $WORKSPACE/slug-bench
+  mkdir -p $WORKSPACE/node-bench
+  mkdir -p $WORKSPACE/rust-bench
+  mkdir -p $WORKSPACE/cf-bench
 
-# install brew and tools
+  cd $HOME/workspace/slug-bench
+  git clone --recursive git@github.com:slugbyte/config.git 
+  cd config
+  source ./config/.bashrc
+  source ./plugin/dot.plugin.sh && dot sync 
+  source ./install/base.sh
 
-WORKSPACE=$HOME/workspace
-mkdir -p $WORKSPACE/slug-bench
-mkdir -p $WORKSPACE/node-bench
-mkdir -p $WORKSPACE/rust-bench
-mkdir -p $WORKSPACE/cf-bench
-
-cd $HOME/workspace/slug-bench
-git clone --recursive git@github.com:slugbyte/config.git 
-cd config
-source ./config/.bashrc
-source ./plugin/dot.plugin.sh
-
-dot sync 
+  select install_file in `ls ./install |grep -v base`; do 
+    source ./install/$install_file
+    break
+  done
+  echo DOT INSTALLED
+}
