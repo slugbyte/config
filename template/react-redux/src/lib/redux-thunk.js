135c658f88017b2c2d019f1b76cbbc5f38b364c9
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-21 11:52:20 +0800

    only proj is now basic vanilla js

--- template/react-redux/src/lib/redux-thunk.js
+++
@@ -1,5 +0,0 @@
-export default (store) => (next) => (action) => {
-  return typeof action === 'function'
-  ? action(store.dispatch, store.getState)
-  : next(action)
-}
--- template/react-redux/src/lib/store-create.js
+++
@@ -1,6 +0,0 @@
-import reducer from '../reducer'
-import thunk from './redux-thunk.js'
-import reporter from './redux-reporter.js'
-import {createStore, applyMiddleware} from 'redux'
-
-export default () => createStore(reducer, applyMiddleware(thunk, reporter))
