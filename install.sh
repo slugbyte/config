ae5af8c0d5f6f727f8b40975c15ad155515dfe98
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-26 21:32:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,16 @@ mkdir -p $WORKSPACE/node-bench
 mkdir -p $WORKSPACE/rust-bench
 mkdir -p $WORKSPACE/cf-bench
 
+echo "INSTALLING BASE"
+source ./install/base.sh
+
+echo "CHOOSE AN INSTALLER (n for none)"
+select install_file in `ls ./install |grep -v base`; do 
+  [[ -n $install_file ]] && source ./install/$install_file
+  break
+done
+echo DOT INSTALLED
+
 
 echo "ENSURE DOT REPO"
 cd $HOME/workspace/slug-bench
@@ -17,11 +27,4 @@ echo "SOURCE DOT AND SYNC"
 cd config
 source ./config/.bashrc
 source ./plugin/dot.plugin.sh && dot sync 
-source ./install/base.sh
 
-echo "CHOOSE AN INSTALLER (n for none)"
-select install_file in `ls ./install |grep -v base`; do 
-  [[ -n $install_file ]] && source ./install/$install_file
-  break
-done
-echo DOT INSTALLED
