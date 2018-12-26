a43bd75766be94dbe0a2f3e853f25564490132d2
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-26 22:03:29 +0800

    added archbox details

--- install/arch.sh
+++ install/arch.sh
@@ -1,1 +1,1 @@
-pacman --noconfirm -S vim tmux psmisc git wget nmap aria2 fzf htop httpie nvm the_silver_searcher cmake gdb clang
+pacman --noconfirm -S vim tmux psmisc git wget nmap aria2 fzf htop httpie the_silver_searcher cmake gdb clang
