9bd5846346086c6091f39aa7a0073702bf64bf7b
Duncan Marsh <slugbyte@slugbyte.com>
2019-01-20 17:37:46 +0800

    update git_util.sh

--- plug/git-util.sh
+++ plug/git-util.sh
@@ -7,7 +7,7 @@ git_push() {
       return 1
     fi 
     echo "Pushing to $branch $@" 
-    git push origin $branch $@
+    git push origin $branch $@ -v
     return 0
   else
     print "Error: Not a git repository"
@@ -20,7 +20,7 @@ git_pull() {
   local branch
   if (( $# > 0 ));then 
     echo "Pulling from $@"
-    git pull origin $@
+    git pull origin $@ -v
     return 0
   fi 
   if branch=$(git rev-parse --abbrev-ref head 2> /dev/null); then
@@ -29,7 +29,7 @@ git_pull() {
       return 1
     fi 
     echo "Pulling from $branch"
-    git pull origin $branch
+    git pull origin $branch -v
     return 0
   else
     print "Error: Not a git repository"
@@ -38,10 +38,9 @@ git_pull() {
 }
 
 git_commit(){
-  if (( $# > 0 ));then 
-    echo "Pulling from $@"
-    git commit -m $@
-    return 0
-  fi 
-  git commit
+  git commit -v -S $@
+}
+
+git_tag(){
+  git tag -v -s $@
 }
