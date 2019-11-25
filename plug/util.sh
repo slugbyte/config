ab5e81cb750e9ffcaab1bf8d280163f5605ea9fd
Duncan Marsh <slugbyte@slugbyte.com>
2019-11-25 16:46:05 +0800

    added grammarly

--- plug/util.sh
+++ plug/util.sh
@@ -104,3 +104,47 @@ bin_info(){
   fi
   echo "ERROR: no such command"
 }
+
+r_ext(){
+  if [[ -z $1 ]]; then 
+    >2& echo "ERROR: no target supplied" 
+    return -1
+  fi 
+
+  filename="$1"
+  f_len=${#filename}
+  ext="$2"
+  e_len=${#ext}
+  extension=${1:f_len-e_len}
+
+  if [[ "$extension" == "$ext" ]];then 
+    mv $filename "${filename::f_len-e_len}"
+    echo renamed  "${filename::f_len-e_len}"
+  else 
+    mv $filename "${filename}${ext}"
+    echo renamed  "${filename}${ext}"
+  fi 
+}
+
+txtify(){
+  if [[ -z $1 ]]; then 
+    >2& echo "ERROR: no target supplied" 
+    return -1
+  fi 
+  r_ext "$1" ".txt"
+}
+
+backupify() {
+  if [[ -z $1 ]]; then 
+    >2& echo "ERROR: no target supplied" 
+    return -1
+  fi 
+  r_ext "$1" ".backup"
+}
+
+grammarly(){
+  txtify "$1"
+  open "$1.txt" -a /Applications/Grammarly.app
+  sleep 2
+  txtify "$1.txt"
+}
