b91d25813f61d8609f880a6febea8ed417d360ff
Duncan Marsh <slugbyte@slugbyte.com>
2020-10-10 00:44:50 +0800

    [refactor] cleaning up sxhkdrc

--- exec/snap-right.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WIDTH=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-WIDTH=$(calc "int($WIDTH*0.5)-8")
-HEIGHT=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-HEIGHT=$(calc "int($HEIGHT)-8")
-XOFFSET=$(calc "$WIDTH+8")
-wmctrl -r :ACTIVE: -b remove,maximized_horz,sticky
-wmctrl -r :ACTIVE: -e "0,$XOFFSET,0,$WIDTH,$HEIGHT"
--- plug/python.sh
+++ plug/python.sh
@@ -1,13 +1,18 @@
-# init pyenv
+## init pyenv
 export PYENV_VERSION=3.8.5
 export PYENV_ROOT="$HOME/.config/pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv shell $PYENV_VERSION
 
-# utils
-#alias pyoff='deactivate'
-#alias pyon='pipenv shell'
+pyup(){
+  eval "$(pyenv init -)"
+  pyenv shell $PYENV_VERSION
+}
+
+pydown(){
+  eval "$(pyenv init -)"
+  pyenv shell $PYENV_VERSION
+}
+
 if [[ $(uname -s) = 'Darwin' ]];then
  pyclean () {
    find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
@@ -17,3 +22,17 @@ else
    find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
  }
 fi 
+
+
+## utils
+##alias pyoff='deactivate'
+##alias pyon='pipenv shell'
+#if [[ $(uname -s) = 'Darwin' ]];then
+ #pyclean () {
+   #find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
+ #}
+#else
+ #pyclean () {
+   #find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
+ #}
+#fi 
