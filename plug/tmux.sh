7d756a182b4a01ec590aa4d15774f56f13f028ac
Duncan Marsh <slugbyte@slugbyte.com>
2021-12-01 12:24:15 +0800

    [refactor] tmux helper fun and sesson in bottombar

--- plug/tmux.sh
+++ plug/tmux.sh
@@ -1,3 +1,8 @@
 #alias ta='tmux attach-session -t'
 #alias tn='tmux new -s sunset'
 #alias tl='tmux ls'
+
+alias tlist='tmux list-sessions'
+alias tname='tmux rename session -t'
+alias tjoin='tmux attach-session -t'
+alias tswap='tmux switch-client -t'
