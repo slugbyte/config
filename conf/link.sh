8337aef326d5bcd9b468fe9dbe5ec1397f41c02e
Duncan Marsh <slugbyte@slugbyte.com>
2021-12-05 12:37:42 +0800

    [feature] a script for linking config files

---
+++ conf/link.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+config_root="$MOLD_ROOT/conf/config/"
+config_path_tree=$(find $config_root -name '*')
+
+for path in $config_path_tree; do
+  if [[ -d $path ]];then
+    mkdir -p "$HOME/.config/${path#$config_root}" &> /dev/null
+  else
+    echo "linking: .config/${path#$config_root}"
+    ln -f $path "$HOME/.config/${path#$config_root}"
+  fi
+done
+
+home_root="$MOLD_ROOT/conf/home/"
+home_path_tree=$(find $home_root -name '*')
+
+for path in $home_path_tree; do
+  if [[ -d $path ]];then
+    mkdir -p "$HOME/${path#$home_root}" &> /dev/null
+  else
+    echo "linking: ~/${path#$home_root}"
+    ln -f $path "$HOME/${path#$home_root}"
+  fi
+done
