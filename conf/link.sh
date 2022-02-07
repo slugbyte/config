31e9f76aeb1febeb3898d354177b82b7586c2f1d
Duncan Marsh <slugbyte@slugbyte.com>
2022-02-07 12:35:02 +0800

    [feature] conf link script links dirs not files

--- conf/link.sh
+++ conf/link.sh
@@ -6,25 +6,22 @@ if [[ -z "$MOLD_ROOT" ]];then
 fi
 
 config_root="$MOLD_ROOT/conf/config/"
-config_path_tree=$(find $config_root -name '*')
+config_path_tree=$(ls "$config_root")
 
 for path in $config_path_tree; do
-  if [[ -d $path ]];then
-    mkdir -p "$HOME/.config/${path#$config_root}" &> /dev/null
-  else
-    echo "linking: .config/${path#$config_root}"
-    ln -f $path "$HOME/.config/${path#$config_root}"
-  fi
+  echo "linking: .config/${path#"$config_root"}"
+  trash "$HOME/.config/${path#"$config_root"}"
+  ln -sf "${config_root}${path}" "$HOME/.config/${path#"$config_root"}"
 done
 
 home_root="$MOLD_ROOT/conf/home/"
-home_path_tree=$(find $home_root -name '*')
+home_path_tree=$(find "$home_root" -name '*')
 
 for path in $home_path_tree; do
   if [[ -d $path ]];then
-    mkdir -p "$HOME/${path#$home_root}" &> /dev/null
+    mkdir -p "$HOME/${path#"$home_root"}" &> /dev/null
   else
-    echo "linking: ~/${path#$home_root}"
-    ln -f $path "$HOME/${path#$home_root}"
+    echo "linking: ~/${path#"$home_root"}"
+    ln -f "$path" "$HOME/${path#"$home_root"}"
   fi
 done
