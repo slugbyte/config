bc489caebdcf3fe54e0d1f0cc232b46a60f81a4f
Duncan Marsh <slugbyte@slugbyte.com>
2022-07-21 10:28:23 +0800

    [patch] git_push will include tags

--- exec/kiipo_launch.sh
+++ exec/kiipo_launch.sh
@@ -1,31 +1,48 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 ## SETUP ENV
 LOG_DIR="$HOME/workspace/tail"
 mkdir -p "$LOG_DIR"
 
-alias pm2='/home/slugbyte/.config/nvm/versions/node/v16.16.0/bin/pm2'
+LOG_FILE="$LOG_DIR/kiipo_launch.log.txt"
+
+alias pullhead='git pull origin $(git rev-parse --abbrev-ref HEAD)'
+
+pm2() {
+  /home/slugbyte/.config/nvm/versions/node/v16.16.0/bin/pm2 $@ |tee -a "$LOG_FILE"
+}
+
+write(){
+  echo $@ >>&! $LOG_FILE
+}
 
 ## HELPER FUNCTIONS
 log(){
-  echo
-  echo "$(tput setaf 141)[$1]$(tput setaf 105) $2$(tput sgr 0)"
+  write ""
+  write "[$1] $2"
+
+  echo ""
+  echo "$(tput setaf 141)[$1]$(tput setaf 105) $2$(tput sgr 0)"  
 }
 
 err(){
+  write ""
+  write "($1) $2" 
+
   echo
-  echo "$(tput setaf 1)[$1]$(tput setaf 209) $2$(tput sgr 0)"
+  echo "$(tput setaf 141)[$1]$(tput setaf 105) $2$(tput sgr 0)"  
 }
 
 update_project(){
+  pwd
   log "GIT PULL" "$1"
-  git pull || {
+  pullhead | tee -a "$LOG_FILE" || {
     err "ERROR" "failled to git pull ($1)"
     exit 1
   }
 
   log "NPM INSTALL" "$1"
-  npm i --force  || {
+  npm i --force | tee -a "$LOG_FILE" || {
     err "ERROR" "failled to npm install ($1)"
     exit 1
   }
@@ -39,7 +56,10 @@ goto() {
 }
 
 ## MAIN
+/bin/rm $LOG_FILE
+
 log "START" "launching and restarting all kiipo demons"
+log "DATE" "$(date)"
 
 ## KILLALL KIIPOO DAEMONS
 log "RESET" "stoping all running kiipo daemons"
@@ -52,7 +72,8 @@ log "LAUNCHING" "ADMIN PORTAL BACKEND"
 pm2 start "bin/www" \
   --name labfront_admin_portal_backend \
   -o "$LOG_DIR/labfront_admin_portal_backend.stdout.txt" \
-  -e "$LOG_DIR/labfront_admin_portal_backend.stderr.txt"
+  -e "$LOG_DIR/labfront_admin_portal_backend.stderr.txt" \
+  --force
 
 ## ADMIN PORTAL FRONTEND
 goto "/opt/legacy/labfront_admin_portal_frontend"
@@ -61,4 +82,5 @@ log "LAUNCHING" "ADMIN PORTAL FRONTEND"
 pm2 start "bin/serve.js" \
   --name labfront_admin_portal_frontend \
   -o "$LOG_DIR/labfront_admin_portal_frontend.stdout.txt" \
-  -e "$LOG_DIR/labfront_admin_portal_frontend.stderr.txt"
+  -e "$LOG_DIR/labfront_admin_portal_frontend.stderr.txt" \
+  --force
