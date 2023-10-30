c65c235fb50436d2fffa2d83a31d6f5b3aa191e1
Duncan Marsh <slugbyte@slugbyte.com>
2023-10-30 13:38:58 +0800

    overhaul link.sh

--- conf/link.sh
+++ conf/link.sh
@@ -1,29 +1,77 @@
 #!/usr/bin/env bash
 
 WORKSPACE_DIR="$HOME/workspace"
-TRASH_DIR="$HOME/.trash"
+TRASH_DIR="$HOME/.Trash"
+ARG="$1"
 
-trash(){
-  mv $1 $TRASH_DIR/
+echo "[BLOINGO]"
+echo "   WORKSPACE_DIR=$WORKSPACE_DIR"
+echo "   TRASH_DIR=$TRASH_DIR"
+
+if [[ ! -d "$WORKSPACE_DIR" ]];then
+  echo "ABORT: DOES NOT EXIST"
+  echo "    $WORKSPACE_DIR"
+  exit 1
+fi
+
+if [[ ! -d "$TRASH_DIR" ]];then
+  echo "ABORT: TRASH_DIR DOES NOT EXIST"
+  echo "   $TRASH_DIR"
+  exit 1
+fi
+
+exec_if_plz(){
+  # place exec_if_plz before line to exec_if_plz it
+  # example ```$ exec_if_plz mv $1  $TRASH_DEST_PATH```
+  if [[ "$ARG" = "--plz" ]];then
+    $@
+  else
+    echo "    [MOCK]" $@
+  fi
 }
 
-config_root="$WORKSPACE_DIR/conf/config/"
-config_path_tree=$(ls "$config_root")
-
-for path in $config_path_tree; do
-  echo "linking: .config/${path#"$config_root"}"
-  trash "$HOME/.config/${path#"$config_root"}"
-  ln -sf "${config_root}${path}" "$HOME/.config/${path#"$config_root"}"
-done
-
-HOME_ROOT="$WORKSPACE_DIR/conf/home/"
-HOME_DOTFILE_LIST=$(find "$HOME_ROOT" -name '*')
-
-for path in $HOME_DOTFILE_LIST; do
-  if [[ -d $path ]];then
-    mkdir -p "$HOME/${path#"$HOME_ROOT"}" &> /dev/null
-  else
-    echo "linking: ~/${path#"$HOME_ROOT"}"
-    ln -f "$path" "$HOME/${path#"$HOME_ROOT"}"
+backup_if_exist(){
+  if [[ -e "$1" ]]; then
+    TRASH_DATE=$(date "+%Y%m%d%H%M%S")
+    TRASH_SOURCE_NAME=$(basename $1)
+    TRASH_DEST_PATH="$TRASH_DIR/config-backup_if_exist-$TRASH_DATE.$TRASH_SOURCE_NAME"
+    exec_if_plz mv $1  $TRASH_DEST_PATH
+    echo "    [BACKUP] $1 -> $TRASH_DEST_PATH"
   fi
-done
+}
+
+link_config(){
+  SOURCE_DIR="$1"
+  SOURCE_FILE_LIST=$(ls -a $SOURCE_DIR | grep -v "^\.*$" | grep -v .DS_Store)
+  DEST_DIR="$2"
+  echo
+  echo "[SOURCE_DIR] $SOURCE_DIR"
+  for SOURCE_FILE in $SOURCE_FILE_LIST; do
+    LINK_SOURCE="$SOURCE_DIR/$SOURCE_FILE"
+    LINK_TARGET="$DEST_DIR/$SOURCE_FILE"
+    echo "  $SOURCE_FILE: $LINK_SOURCE"
+    backup_if_exist "$LINK_TARGET"
+    if [[ -d "$LINK_SOURCE" ]];then 
+      exec_if_plz ln -sf "$LINK_SOURCE" "$LINK_TARGET"
+      echo "    [SOFT_LINK] $LINK_TARGET -> $LINK_SOURCE"
+    else
+      exec_if_plz ln -f "$LINK_SOURCE" "$LINK_TARGET"
+      echo "    [HARD_LINK] $LINK_TARGET -> $LINK_SOURCE"
+    fi
+  done
+}
+
+link_config "$WORKSPACE_DIR/conf/config" "$HOME/.config"
+link_config "$WORKSPACE_DIR/conf/home" "$HOME"
+
+if [[ $ARG = "--plz" ]];then
+  echo
+  echo "[GLORB]"
+  echo "    $RANDOM is a lucky number, don't $(whoami) think?"
+  echo "    GLORB work is complete."
+else
+  echo
+  echo "[GLORB]"
+  echo "   GLORB will do NO work without magic words."
+  echo "   Now wait $(whoami) run | $ link.sh --plz | , ok?"
+fi
