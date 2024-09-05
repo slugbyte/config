4b37a79aeb1b70beb3d5432aa5b118eee521e948
Duncan Marsh <slugbyte@slugbyte.com>
2024-09-05 22:14:15 +0800

    update lualine file format and colors

--- config/nvim/lua/slugbyte/plugin/lualine.lua
+++ config/nvim/lua/slugbyte/plugin/lualine.lua
@@ -5,6 +5,7 @@ return {
     },
     config = function()
         local unruly_worker = require("unruly-worker")
+        local lackluster = require("lackluster")
 
         require("lualine").setup({
             options = {
@@ -15,11 +16,11 @@ return {
             },
             sections = {
                 lualine_a = { "mode" },
-                lualine_b = { "filename" },
+                lualine_b = { { "filename", path = 4 } },
                 lualine_c = {},
-                lualine_x = { "navic" },
-                lualine_y = { "searchcount", "selectioncount", "diagnostics", "filetype", "branch" },
-                lualine_z = { unruly_worker.hud.generate },
+                lualine_x = {},
+                lualine_y = { "searchcount", "selectioncount", "diagnostics", { "branch", color = { fg = lackluster.color.gray6 } }, "diff" },
+                lualine_z = { { unruly_worker.hud.generate, color = { fg = lackluster.color.gray5 } } },
             },
         })
     end,
